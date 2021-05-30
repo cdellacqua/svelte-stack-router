e896bba9ccd024ac83e50c0c26e4f8c55d073969
Carlo Dell'Acqua <carlo.dellacqua97@gmail.com>
2021-05-30 19:50:31 +0200

    export utils

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export * from './stack-router';
 export * from './transition-functions';
 export * from './types';
+export * from './utils';
 export { default as StackRouter } from './StackRouter.svelte';
